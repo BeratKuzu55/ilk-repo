00b250468029ee8523d2e8ff1e82fbdfe97d9726
BeratKuzu55 <75533278+BeratKuzu55@users.noreply.github.com>
2020-12-09 13:13:07 +0300

    asal sayı kontrol

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+
+    int sayi;
+    int sayi2;
+    printf("lutfen 1 ile 10 arasinda bir sayi giriniz : \n");
+    scanf("%d",&sayi);
+
+    if (sayi >= 1 && sayi <= 10)
+    {
+      switch (sayi)
+      {
+          case 2:
+          case 3:
+          case 5:
+          case 7:printf("asal sayi girdiniz : \n");
+          break;
+          default:printf("girdiginiz sayi asal degildir : \n");
+      }
+    }
+    else printf("aralik disi bir deger girdiniz : \n");
+//sunularda asal sayi uygulamasi var ona bak !!!
+    return 0;
+}
