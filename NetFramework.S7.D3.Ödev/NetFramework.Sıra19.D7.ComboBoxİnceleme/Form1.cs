cbf637b3bd45dfa5ecb09f138d137f78871bfc94
BeratKuzu55 <75533278+BeratKuzu55@users.noreply.github.com>
2020-12-09 14:02:56 +0300

    Update Form1.cs

--- NetFramework.S7.D3.Ödev/NetFramework.Sıra19.D7.ComboBoxİnceleme/Form1.cs
+++ NetFramework.S7.D3.Ödev/NetFramework.Sıra19.D7.ComboBoxİnceleme/Form1.cs
@@ -19,16 +19,14 @@ namespace NetFramework.Sıra19.D7.ComboBoxİnceleme
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //items.Add() metodu kullanılarak değerler combo box a eklendi 1.yöntem buydu 
-            //comboBoxDoldur();
+            
 
             comboBoxDoldurV2();
         }
 
         private void comboBoxDoldurV2()
         {
-            //dataSource da items gibi obje alıyor ama almış olduğu objeyi items a içeride eleman olarak 
-            //otomatik ekliyor 
+            
             comboBoxÜrünListe.DataSource = Database.ürünTablo;
         }
 
@@ -48,15 +46,7 @@ namespace NetFramework.Sıra19.D7.ComboBoxİnceleme
         private void comboBoxÜrünListe_SelectedIndexChanged(object sender, EventArgs e)
         {
 
-            /*
-             *object ten sender ne demek ? => kullanıcı formun üzerinde hangi form elemanıyla işlem 
-             * yaptıysa ilgili form elemanı bize parametre olarak sender üzerinden gelir 
-             * mesela combo box tan bir şey sectiğinde sender da onu görürüz 
-             */
-
-            /*
-             * EventArgs e nedir ? işlemin ne olduğunu bize söyler 
-             */
+           
 
 
 
@@ -70,17 +60,6 @@ namespace NetFramework.Sıra19.D7.ComboBoxİnceleme
 
 
 
-            //2. yöntem 
-
-            // ComboBox seçilenComboBox = sender as ComboBox;
-
-            // 1. yöntem eğer form içerisinde bulunan form elemanına özel bir metot yazıldı ise 
-            //o zaman ilgili elemanınızı direkt olarak çağırmak daha mantıklıdır 
-
-            //2. yöntem çalışma zamanında oluşturmuş olduğumuz form elemanlarından hangisine tıklandığını
-            //veya işlem yapıldığını bilmediğimiz için ve metodumuz birden fazla form elemanında olay metotu 
-            // olarak tanımlandığı için biz sender üzerinden gelen nesneyi kullanır kullanıcının hangi 
-            //windows form elemanında işlem yapmış olduğunu anlarız 
 
 
 
